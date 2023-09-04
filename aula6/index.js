1b0f38a892ade78de0c6de2ade8c2ce8cf82e265
roger <roger@policiacivil.pe.gov.br>
2023-09-04 14:31:58 -0300

    ajustes

--- aula6/index.js
+++ aula6/index.js
@@ -1,7 +1,18 @@
-let nome = 'João'
+let nome // Declarou a variável 
+nome = 'João' // Inicializou a variável
 
 console.log(nome, 'nasceu em 1984.')
 console.log('Em 2000', nome, 'conheceu Maria.')
 console.log(nome, 'casou-se com Maria em 2012.')
 console.log('Maria teve um filho de', nome,'em 2015.')
 console.log('O filho de', nome, 'se chama Eduardo.')
+
+// Criação de variáveis:
+// Não pode usar palavras reservadas para criar variáveis
+// Não pode começar o nome de uma variável com número
+// Não pode conter espaços ou traços
+// Variáveis precisam ter nomes significativos
+// O padrão mais utilizado é o camelCase
+// No JS os nomes são case sensitive 
+// Não pode redeclarar uma variável com mesmo nome no mesmo escopo
+// No JS é mais recomendado utilizar CONST e LET e não utilizar VAR
